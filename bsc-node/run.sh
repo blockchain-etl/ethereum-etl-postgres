46c2520f0daaabbfc75910ec2eccea18c8c4da9f
tebayoso <tebayoso@gmail.com>
2021-04-17 21:32:12 +0200

    Fixed docker

--- bsc-node/run.sh
+++ bsc-node/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
+
 geth --datadir /root/.ethereum init /app/genesis.json
+
 geth --cache=4096 --rpc --rpcaddr 0.0.0.0 --rpcapi personal,admin,db,eth,net,web3,miner,shh,txpool,debug --ws --config /app/config.toml --datadir /root/.ethereum
