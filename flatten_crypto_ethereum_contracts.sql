3566d3007ecffa925ecad32f175144401bbbf7d3
Wayland He <waylandhe@gmail.com>
2023-03-09 14:21:12 -0800

    set up month worth of data to be uploaded to our psql instances

--- flatten_crypto_ethereum_contracts.sql
+++ flatten_crypto_ethereum_contracts.sql
@@ -7,6 +7,7 @@ select
     concat('{', array_to_string(function_sighashes, ','), '}') as function_sighashes,
     is_erc20,
     is_erc721,
+    block_hash,
     block_number,
     block_timestamp
 from `bigquery-public-data.crypto_ethereum.contracts`
